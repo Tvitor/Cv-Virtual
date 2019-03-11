46fac996b5d1f76950b057cf2398e7860632e1ad
tvitor <contato.vst@gmail.com>
2019-03-11 14:24:18 -0300

    primeiro componente

--- cv-virtual/src/app/app-routing.module.ts
+++ cv-virtual/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { SobremimComponent} from './sobremim/sobremim.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',
+    redirectTo: 'sobremim', pathMatch: 'full'},
+    { path: 'sobremim', component: SobremimComponent }
+   ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+             CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ cv-virtual/src/app/sobremim/sobremim.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SobremimComponent } from './sobremim.component';
+
+describe('SobremimComponent', () => {
+  let component: SobremimComponent;
+  let fixture: ComponentFixture<SobremimComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SobremimComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SobremimComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
