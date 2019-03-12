198576aad29db657718bdf1bb2ee82e2ae82021c
tvitor <contato.vst@gmail.com>
2019-03-12 20:53:24 -0300

    redirecto

--- cv-virtual/src/app/app-routing.module.ts
+++ cv-virtual/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import { ContatoComponent } from './contato/contato.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SobremimComponent } from './sobremim/sobremim.component';
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home'
-  }
+    {
+      path:"",redirectTo: "home", pathMatch: 'full'
+    },
+      {path:"home",component: HomeComponent},
+      {path:"sobremim",component: SobremimComponent},
+      {path:"portfolio",component:PortfolioComponent},
+      {path:"contato",component: ContatoComponent},
    ]
 
 @NgModule({
